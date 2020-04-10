a8d92063a90de3d08be5f279645b3fb76cc3ee04
Marcjanna Mietla <mietla.m@gmail.com>
2020-04-10 13:18:19 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,7 +3,10 @@
 #  puts "   |   |   "
 #  puts "-----------"
 #  puts "   |   |   "
-def display_board
-board = [" "," "," "]
+def display_board(board: nil)
 puts " #{board[0]} | #{board[1]} | #{board[2]} "
+puts "-----------"
+puts " #{board[3]} | #{board[4]} | #{board[5]} "
+puts "-----------"
+puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
